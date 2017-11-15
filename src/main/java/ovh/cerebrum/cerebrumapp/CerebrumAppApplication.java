3ad603307eacd8bb97c073223b62c59fca93cca2
heimdall <hemidal@gmail.com>
2017-11-15 21:08:25 +0100

    init app

---
+++ src/main/java/ovh/cerebrum/cerebrumapp/CerebrumAppApplication.java
@@ -0,0 +1,12 @@
+package ovh.cerebrum.cerebrumapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerebrumAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerebrumAppApplication.class, args);
+	}
+}
---
+++ src/test/java/ovh/cerebrum/cerebrumapp/CerebrumAppApplicationTests.java
@@ -0,0 +1,16 @@
+package ovh.cerebrum.cerebrumapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CerebrumAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
