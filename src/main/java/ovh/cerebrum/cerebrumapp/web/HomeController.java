ef119a9e79f1d2b7f6f0ca88f061ac5413b36a42
Pawel Grudzien <erzetpe@gmail.com>
2018-01-22 22:42:28 +0100

    U: #3 Hello World D: Added CORS to application

--- src/main/java/ovh/cerebrum/cerebrumapp/web/HomeController.java
+++ src/main/java/ovh/cerebrum/cerebrumapp/web/HomeController.java
@@ -1,6 +1,7 @@
 package ovh.cerebrum.cerebrumapp.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class HomeController {
         this.greetingService = greetingService;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/greeting")
     public Greeting homeSite() {
         Optional<Greeting> greetingOpt =this.greetingService.findById(1);
