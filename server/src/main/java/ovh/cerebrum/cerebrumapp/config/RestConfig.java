953b5012cca445ca0f2933dc04b5a71bde413211
heimdall <hemidal@gmail.com>
2018-02-28 21:42:22 +0100

    #4 fixing issue with CORS

--- server/src/main/java/ovh/cerebrum/cerebrumapp/config/RestConfig.java
+++ server/src/main/java/ovh/cerebrum/cerebrumapp/config/RestConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class RestConfig {
-    
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
