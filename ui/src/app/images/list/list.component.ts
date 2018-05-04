731888efd508421075bf507311e2eabecdcfe622
Pawel Grudzien <erzetpe@gmail.com>
2018-05-05 01:15:42 +0200

    Issue: #6 Description: Added display dummy data to angular list component

--- ui/src/app/images/list/list.component.ts
+++ ui/src/app/images/list/list.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  images = [{osType: 'Ubuntu', osVersion: '16.04', additionalSoftware: ''},
+            {osType: 'Ubuntu', osVersion: '17.10', additionalSoftware: ''},
+            {osType: 'Ubuntu', osVersion: '18.04', additionalSoftware: ''}];
+
   constructor() { }
 
   ngOnInit() {
