45f65ba3e3a62d4f26186f21d18ef75e051d61ba
Pawel Grudzien <erzetpe@gmail.com>
2018-05-05 09:51:49 +0200

    Issue: #6 Description: Added displaying additonal software for dummy data

--- ui/src/app/images/list/list.component.ts
+++ ui/src/app/images/list/list.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  images = [{osType: 'Ubuntu', osVersion: '16.04', additionalSoftware: ''},
-            {osType: 'Ubuntu', osVersion: '17.10', additionalSoftware: ''},
-            {osType: 'Ubuntu', osVersion: '18.04', additionalSoftware: ''}];
+  images = [{osType: 'Ubuntu', osVersion: '16.04', additionalSoftware: ['Java', 'Spring Boot']},
+            {osType: 'Ubuntu', osVersion: '17.10', additionalSoftware: 'PostgreSQL'},
+            {osType: 'Ubuntu', osVersion: '18.04', additionalSoftware: ''},
+            {osType: 'CentOS', osVersion: '7.3', additionalSoftware: 'Python'}];
 
   constructor() { }
 
