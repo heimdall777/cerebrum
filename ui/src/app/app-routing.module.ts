4b28095afbb3b2d8820469443272b4e68903f4b1
Pawel Grudzien <erzetpe@gmail.com>
2018-05-21 22:32:37 +0200

    Issue: #13 Description: Added site for 404 not found

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { GreetingsComponent } from './greetings/greetings.component';
 import { ListComponent } from './images/list/list.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
 
 const routes: Routes = [
     { path: '', component: GreetingsComponent },
-    { path: 'images', component: ListComponent }
+    { path: 'images', component: ListComponent },
+    { path: '**', component:  NotFoundComponent }
 ];
 
 @NgModule({
