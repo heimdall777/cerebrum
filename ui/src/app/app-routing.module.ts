b242f4545b2a63ddde2f57a1324b80c1287d5f36
Pawel Grudzien <erzetpe@gmail.com>
2018-05-31 00:53:23 +0200

    Issue: #14 Description: Added general error site

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { GreetingsComponent } from './greetings/greetings.component';
 import { ListComponent } from './images/list/list.component';
 import { NotFoundComponent } from './error/not-found/not-found.component';
+import { GeneralErrorComponent } from './error/general-error/general-error.component';
 
 const routes: Routes = [
     { path: '', component: GreetingsComponent },
     { path: 'images', component: ListComponent },
+    { path: 'error', component: GeneralErrorComponent },
     { path: '**', component:  NotFoundComponent }
 ];
 
