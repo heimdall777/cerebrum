cd9636928bd0e22ea4a9c0aa2a2bf0fea81f7651
Pawel Grudzien <erzetpe@gmail.com>
2018-07-10 22:07:07 +0200

    Issue: #7 Description: Add API for container creation

--- server/src/main/java/ovh/cerebrum/cerebrumapp/web/DockerImageController.java
+++ server/src/main/java/ovh/cerebrum/cerebrumapp/web/DockerImageController.java
@@ -1,10 +1,11 @@
 package ovh.cerebrum.cerebrumapp.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ovh.cerebrum.cerebrumapp.domain.DockerImage;
+import ovh.cerebrum.cerebrumapp.exception.AlreadyExistException;
 import ovh.cerebrum.cerebrumapp.service.DockerImageService;
 
 import java.util.List;
@@ -23,6 +24,35 @@ public class DockerImageController {
     @GetMapping("")
     public List<DockerImage> getAllImages(){
         return this.dockerImageService.findAll();
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<String> createImage(@RequestBody DockerImage dockerImage) {
+
+        log.info("Adding docker container to repository.");
+
+        if (!isContainerValid(dockerImage)) {
+            log.info("Cannot create docker container. Bad request." + dockerImage.toString());
+            return new ResponseEntity<String>("Cannot create docker container.", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            dockerImageService.save(dockerImage);
+            return new ResponseEntity<String>("Docker container added to repository.", HttpStatus.OK);
+        } catch (AlreadyExistException e) {
+            log.error("Object already in container repository.", e);
+            return new ResponseEntity<String>("Object already in container repository.", HttpStatus.CONFLICT);
+        }
+
+    }
+
+    private boolean isContainerValid(DockerImage dockerImage) {
+
+        if ((dockerImage.getOsType() == null) && dockerImage.getOsVersion() == null) {
+            return false;
+        }
+
+        return true;
     }
 
 }
